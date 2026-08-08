1b0847ef765b57c33b47bb5e707a02993d95e21d
bands001-app <banderanigel@gmail.com>
2026-08-08 23:27:37 +0100

    Move modules into vtsmom package so imports work

---
+++ vtsmom/__init__.py
@@ -0,0 +1,4 @@
+from vtsmom.backtest import BacktestConfig, run_backtest
+from vtsmom.costs import CostModel
+from vtsmom.metrics import performance_summary
+from vtsmom.walkforward import WalkForwardConfig, walk_forward
